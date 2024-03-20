763187123223b1862a200dfba0843f3e717c742a
Glitch (glitch-hello-website) <none>
2024-03-20 14:26:16 +0000

    🏙🚌 Checkpoint ./index.html:66041770/569:66042137/813 ./script.js:66041770/5554 ./.glitch-assets:66042137/411

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ let aciertos = 0;
 
 /*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 1*/
 const respuestaCorrecta = document.getElementById("answer-3");
-const respuestaIncorrecta = document.getElementById("answer-1");
+const respuestaIncorrecta1 = document.getElementById("answer-1");
 const respuestaIncorrecta2 = document.getElementById("answer-2");
 
 
@@ -45,19 +45,81 @@ function evaluarRespuestaP1(respuesta){
   if(respuesta==3){
       aciertos++;
       respuestaCorrecta.classList.add("correct");
-      respuestaIncorrecta.classList.add("incorrect");
+      respuestaIncorrecta1.classList.add("incorrect");
       respuestaIncorrecta2.classList.add("incorrect");
       console.log(aciertos);
   } else {
       respuestaCorrecta.classList.add("correct");
-      respuestaIncorrecta.classList.add("incorrect");
+      respuestaIncorrecta1.classList.add("incorrect");
       respuestaIncorrecta2.classList.add("incorrect");
   }
   
-  // Ocultar la pregunta actual y mostrar la siguiente después de 30 segundos
+  /*Ocultar la pregunta actual y mostrar la siguiente después de 30 segundos
     setTimeout(function() {
         document.getElementById('cientificasPreguntaUno').style.display = "none";
         document.getElementById('cientificasPreguntaDos').style.display = "block";
-    }, 1000); 
+    }, 1000);*/
   
 }
+
+function preguntaDosCientificas(){
+  document.getElementById('cientificasPreguntaUno').style.display = "none";
+  document.getElementById('cientificasPreguntaDos').style.display = "block";
+}
+
+
+/*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 2*/
+const respuestaIncorrecta4 = document.getElementById("answer-4");
+const respuestaCorrecta5 = document.getElementById("answer-5");
+const respuestaIncorrecta6 = document.getElementById("answer-6");
+
+
+
+function evaluarRespuestaP2(respuesta){
+  if(respuesta==5){
+      aciertos++;
+      respuestaCorrecta5.classList.add("correct");
+      respuestaIncorrecta4.classList.add("incorrect");
+      respuestaIncorrecta6.classList.add("incorrect");
+      console.log(aciertos);
+  } else {
+      respuestaCorrecta5.classList.add("correct");
+      respuestaIncorrecta4.classList.add("incorrect");
+      respuestaIncorrecta6.classList.add("incorrect");
+  }
+  
+}
+
+function preguntaTresCientificas(){
+  document.getElementById('cientificasPreguntaDos').style.display = "none";
+  document.getElementById('cientificasPreguntaTres').style.display = "block";
+}
+
+/*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 3*/
+const respuestaIncorrecta4 = document.getElementById("answer-4");
+const respuestaCorrecta5 = document.getElementById("answer-5");
+const respuestaIncorrecta6 = document.getElementById("answer-6");
+
+
+
+function evaluarRespuestaP2(respuesta){
+  if(respuesta==5){
+      aciertos++;
+      respuestaCorrecta5.classList.add("correct");
+      respuestaIncorrecta4.classList.add("incorrect");
+      respuestaIncorrecta6.classList.add("incorrect");
+      console.log(aciertos);
+  } else {
+      respuestaCorrecta5.classList.add("correct");
+      respuestaIncorrecta4.classList.add("incorrect");
+      respuestaIncorrecta6.classList.add("incorrect");
+  }
+  
+}
+
+function preguntaTresCientificas(){
+  document.getElementById('cientificasPreguntaDos').style.display = "none";
+  document.getElementById('cientificasPreguntaTres').style.display = "block";
+}
+
+
