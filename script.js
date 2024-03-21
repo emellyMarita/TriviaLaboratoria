055b51bffb5987b6f9ead769c339997838a92550
Glitch (glitch-hello-website) <none>
2024-03-21 15:14:12 +0000

    🐑⛵️ Checkpoint ./script.js:66041770/2558:66042137/783

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nombreUsuario = document.getElementById('nombre'); /*<input class="sendname" type="text" id="nombre"
           placeholder="Ingresa tu nombre aquí"/>*/
 
+/*Función que muestra el nombre del usuario en la pantalla y redirige al usuario a la segunda pantalla*/
 function mostrarNombre(){
     const nombre = nombreUsuario.value; /*'diego'*/
     
@@ -10,30 +11,22 @@ function mostrarNombre(){
     document.getElementById('resultados').style.display = "none";
 }
 
+
+/*ELEGIR TEMÁTICA*/
+
+/*Si el usuario elige el tema 'Científicas Peruanas', esta función lo lleva a la pregunta uno de esa temática*/
 function preguntasCientificasPeruanas(){
   document.getElementById('subject').style.display = "none";
   document.getElementById('cientificasPreguntaUno').style.display = "block";
 }
 
+/*Si el usuario elige el tema 'Divulgadoras Peruanas', esta función lo lleva a la pregunta uno de esa temática*/
 function preguntasDivulgadorasPeruanas(){
   document.getElementById('subject').style.display = "none";
   document.getElementById('divulgadorasPreguntaUno').style.display = "block";
 }
 
-function nextone(){
-  document.getElementById('cientificasPreguntaUno').style.display = "none";
-  document.getElementById('cientificasPreguntaDos').style.display = "block";
-}
-
-function nexttwo(){
-  document.getElementById('cientificasPreguntaDos').style.display = "none";
-  document.getElementById('cientificasPreguntaTres').style.display = "block";
-}
-
-function nextFinal(){
-  document.getElementById('cientificasPreguntaTres').style.display = "none";
-  document.getElementById('resultados').style.display = "block";
-}
+/*FLUJO DE PANTALLAS DE DIVULGADORAS PERUANAS*/
 
 function siguienteuno(){
   document.getElementById('divulgadorasPreguntaUno').style.display = "none";
@@ -52,16 +45,18 @@ function siguientetres(){
 }
 
 
-/* EVALUAR RESPUESTA */
+/* EVALUACIÓN DE RESPUESTAS */
+
 let aciertos = 0;
 
-/*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 1*/
+/*CIENTIFICAS PERUANAS*/
+
+/*Mapeamos los botones de la pregunta 1*/
 const respuestaCorrecta = document.getElementById("answer-3");
 const respuestaIncorrecta1 = document.getElementById("answer-1");
 const respuestaIncorrecta2 = document.getElementById("answer-2");
 
-
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP1(respuesta){
   if(respuesta==3){
       aciertos++;
@@ -74,50 +69,58 @@ function evaluarRespuestaP1(respuesta){
       respuestaCorrecta.classList.add("correct");
       respuestaIncorrecta1.classList.add("incorrect");
       respuestaIncorrecta2.classList.add("incorrect");
+      bloquearBotonesP1();
   }
-  
   /*Ocultar la pregunta actual y mostrar la siguiente después de 30 segundos
     setTimeout(function() {
         document.getElementById('cientificasPreguntaUno').style.display = "none";
         document.getElementById('cientificasPreguntaDos').style.display = "block";
     }, 1000);*/
-  
 }
 
+/*Función que bloquea botones de la pregunta 1*/
 function bloquearBotonesP1(){
   respuestaCorrecta.disabled = true;
-
+  respuestaIncorrecta1.disabled = true;
+  respuestaIncorrecta2.disabled = true;
 }
-
-
 
 
 function preguntaDosCientificas(){
-  
   document.getElementById('cientificasPreguntaUno').style.display = "none"; 
   document.getElementById('cientificasPreguntaDos').style.display = "block";
 }; 
 
 
 
-/*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 2*/
+/*CIENTIFICAS PERUANAS*/
+/*Mapeamos los botones de la pregunta 2*/
 const respuestaIncorrecta4 = document.getElementById("answer-4");
 const respuestaCorrecta5 = document.getElementById("answer-5");
 const respuestaIncorrecta6 = document.getElementById("answer-6");
 
 
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP2(respuesta){
   if(respuesta==5){
+      aciertos++;
       respuestaCorrecta5.classList.add("correct");
       respuestaIncorrecta4.classList.add("incorrect");
       respuestaIncorrecta6.classList.add("incorrect");
-      aciertos++;
       console.log(aciertos);
+      bloquearBotonesP2();
+    
   } else {
       respuestaCorrecta5.classList.add("correct");
       respuestaIncorrecta4.classList.add("incorrect");
       respuestaIncorrecta6.classList.add("incorrect");
+      bloquearBotonesP2();
+         
+    }
+      function bloquearBotonesP2(){
+      respuestaCorrecta5.disabled = true;
+      respuestaIncorrecta6.disabled = true;
+      respuestaIncorrecta7.disabled = true;
   }
   
 }
@@ -127,13 +130,15 @@ function preguntaTresCientificas(){
   document.getElementById('cientificasPreguntaTres').style.display = "block";
 }
 
-/*CIENTIFICAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 3*/
+/*CIENTIFICAS PERUANAS*/
+
+/*Mapeamos los botones de la pregunta 3*/
 const respuestaIncorrecta7 = document.getElementById("answer-7");
 const respuestaCorrecta8 = document.getElementById("answer-8");
 const respuestaIncorrecta9 = document.getElementById("answer-9");
 
 
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP3(respuesta){
   if(respuesta==8){
       respuestaCorrecta8.classList.add("correct");
@@ -149,13 +154,15 @@ function evaluarRespuestaP3(respuesta){
   
 }
 
-/*DIVULGADORAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 1*/
+/*DIVULGADORAS PERUANAS*/
+
+/*Mapeamos los botones de la pregunta 1*/
 const respuestaCorrecta10 = document.getElementById("answer-10");
 const respuestaIncorrecta11 = document.getElementById("answer-11");
 const respuestaIncorrecta12 = document.getElementById("answer-12");
 
 
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP4(respuesta){
   if(respuesta==10){
       respuestaCorrecta10.classList.add("correct");
@@ -171,13 +178,15 @@ function evaluarRespuestaP4(respuesta){
   
 }
 
-/*DIVULGADORAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 2*/
+/*DIVULGADORAS PERUANAS*/
+
+/*Mapeamos los botones de la pregunta 2*/
 const respuestaCorrecta13 = document.getElementById("answer-13");
 const respuestaIncorrecta14 = document.getElementById("answer-14");
 const respuestaIncorrecta15 = document.getElementById("answer-15");
 
 
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP5(respuesta){
   if(respuesta==14){
       respuestaCorrecta13.classList.add("incorrect");
@@ -194,12 +203,14 @@ function evaluarRespuestaP5(respuesta){
 }
 
 /*DIVULGADORAS PERUANAS -> MAPEO BOTONES DE PREGUNTA 3*/
+
+/*Mapeamos los botones de la pregunta 3*/
 const respuestaIncorrecta16 = document.getElementById("answer-16");
 const respuestaIncorrecta17 = document.getElementById("answer-17");
 const respuestaCorrecta18 = document.getElementById("answer-18");
 
 
-
+/*Evalúa las respuestar insertadas por el usuario*/
 function evaluarRespuestaP6(respuesta){
   if(respuesta==18){
       respuestaIncorrecta16.classList.add("incorrect");
@@ -214,13 +225,6 @@ function evaluarRespuestaP6(respuesta){
     }
   }
   
-  /*
-  function deshabilitar(respuesta){
-  if (aciertos==1){
-    evaluarRespuestaP1.disable=true;
-}
-
-  */
 
 function irFinal(){
   document.getElementById('cientificasPreguntaTres').style.display = "none";
